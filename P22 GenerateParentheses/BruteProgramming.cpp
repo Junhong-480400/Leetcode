51669ada4bd2a0ac120721ad3ffd109cd18c27d3
Junhong Wu <53637206+Junhong-480400@users.noreply.github.com>
2020-10-31 18:20:22 +0800

    Create BruteProgramming.cpp

---
+++ P22 GenerateParentheses/BruteProgramming.cpp
@@ -0,0 +1,42 @@
+class Solution {
+    bool valid(const string& str) {
+        //判断str是否是合格的括号组合
+        int balance = 0;
+        for (char c : str) {
+            if (c == '(') {
+                ++balance;
+            } else {
+                --balance;
+            }
+            if (balance < 0) {
+                return false;
+            }
+        }
+        return balance == 0;
+    }
+
+    void generate_all(string& current, int n, vector<string>& result) {
+        //生成所有可能的括号组合，再判断其中合格的组合，并把它们放入字符串向量result中
+        //本函数采取了递归，过程比较复杂，可意会
+        if (n == current.size()) {
+            if (valid(current)) {
+                result.push_back(current);
+            }
+            return;
+        }
+        current += '(';
+        generate_all(current, n, result);
+        current.pop_back();
+        current += ')';
+        generate_all(current, n, result);
+        current.pop_back();
+    }
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        string current;
+        generate_all(current, n * 2, result);
+        return result;
+    }
+};
+
