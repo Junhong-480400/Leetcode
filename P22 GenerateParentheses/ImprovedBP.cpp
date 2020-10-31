6f951dadbfe53eedbbb8e6c0ce41a47f5d463d84
Junhong Wu <53637206+Junhong-480400@users.noreply.github.com>
2020-10-31 18:23:54 +0800

    Create ImprovedBP.cpp

---
+++ P22 GenerateParentheses/ImprovedBP.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    void backtrack(vector<string>& ans, string& cur, int open, int close, int n) {
+        //（先判断）当左括号小于n，放入左括号；（再判断）当右括号小于n，放入右括号
+        //最终输出结果
+        if (cur.size() == n * 2) {
+            ans.push_back(cur);
+            return;
+        }
+        if (open < n) {
+            cur.push_back('(');
+            backtrack(ans, cur, open + 1, close, n);
+            cur.pop_back();
+        }
+        if (close < open) {
+            cur.push_back(')');
+            backtrack(ans, cur, open, close + 1, n);
+            cur.pop_back();
+        }
+    }
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        string current;
+        backtrack(result, current, 0, 0, n);
+        return result;
+    }
+};
